1b95db81b58ab672cd5500a0b6e67ad0776bd64b
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-18 11:07:26 -0600

    Distillation cfg validation

--- examples/on_policy_distillation_trainer/run_qwen2_5-05b_student-7b_teacher_gsmk8k.sh
+++ examples/on_policy_distillation_trainer/run_qwen2_5-05b_student-7b_teacher_gsmk8k.sh
@@ -4,7 +4,7 @@ conda activate verl
 export PATH=$CONDA_PREFIX/bin:$PATH
 export NCCL_P2P_DISABLE=1
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=4,5
 export DATA_PATH=$PWD/../verlData
 export HF_HOME=$DATA_PATH
 export VLLM_CACHE_DIR=$DATA_PATH/vllm_cache
@@ -16,68 +16,72 @@ set -xeuo pipefail
 
 ############################ Quick Config ############################
 
-rollout_name="vllm" # sglang or vllm
-project_name='verl_on_policy_distillation_example_gsm8k'
-exp_name='qwen2_5-05b_student-7b_teacher'
+ROLLOUT_NAME="vllm" # sglang or vllm
+PROJECT_NAME='verl_on_policy_distillation_example_gsm8k'
+FAMILY="Qwen"
+STUDENT_MODEL=Qwen2.5-0.5B
+TEACHER_MODEL=Qwen2.5-0.5B-Instruct
+DISTILLATION_LOSS_MODE="jsd_topk"
+EXP_NAME="${FAMILY}/student-${STUDENT_MODEL}/teacher-${TEACHER_MODEL}/loss-${DISTILLATION_LOSS_MODE}"
 
-max_prompt_length=256
-max_response_length=512
-train_prompt_bsz=128
+MAX_PROMPT=256
+MAX_RESPONSE_LENGTH=512
+TRAIN_PROMPT_BSZ=128
+MICRO_BATCH_SIZE=1
+
+DP_SIZE=1
 
 ############################ Paths ############################
 
 gsm8k_train_path=$DATA_PATH/gsm8k/train.parquet
 gsm8k_test_path=$DATA_PATH/gsm8k/test.parquet
-math_train_path=$DATA_PATH/math/train.parquet
-math_test_path=$DATA_PATH/math/test.parquet
 
-train_files="['$gsm8k_train_path']"
-test_files="['$gsm8k_test_path']"
+TRAIN_FILES="['$gsm8k_train_path']"
+TEST_FILES="['$gsm8k_test_path']"
 
 ############################ Parameter Groups ############################
-MICRO_BATCH_SIZE=2
 
 DATA=(
-    data.train_files="$train_files"
-    data.val_files="$test_files"
-    data.max_prompt_length=$max_prompt_length
-    data.max_response_length=$max_response_length
-    data.train_batch_size=$train_prompt_bsz
+    data.train_files="$TRAIN_FILES"
+    data.val_files="$TEST_FILES"
+    data.max_prompt_length=$MAX_PROMPT
+    data.max_response_length=$MAX_RESPONSE_LENGTH
+    data.train_batch_size=$TRAIN_PROMPT_BSZ
     data.filter_overlong_prompts=True
     data.truncation='error'
     data.shuffle=False
 )
 
 MODEL=(
-    actor_rollout_ref.model.path=Qwen/Qwen2.5-0.5B
+    actor_rollout_ref.model.path="${FAMILY}/${STUDENT_MODEL}"
     actor_rollout_ref.model.enable_gradient_checkpointing=True
 )
 
 DISTILLATION=(
     actor_rollout_ref.distillation_config.enabled=True
-    actor_rollout_ref.distillation_config.loss_mode=jsd_topk
+    actor_rollout_ref.distillation_config.loss_mode=$DISTILLATION_LOSS_MODE
     actor_rollout_ref.distillation_config.jsd_beta=0.5
     actor_rollout_ref.distillation_config.topk=32
-    actor_rollout_ref.distillation_config.teacher_model.path=Qwen/Qwen2.5-7B-Instruct
+    actor_rollout_ref.distillation_config.teacher_model.path="${FAMILY}/${TEACHER_MODEL}"
+    actor_rollout_ref.distillation_config.log_prob_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE
+    actor_rollout_ref.distillation_config.fsdp_config.param_offload=True
 )
 
 ACTOR=(
     actor_rollout_ref.actor.optim.lr=1e-6
-    actor_rollout_ref.actor.ppo_mini_batch_size=$train_prompt_bsz
+    actor_rollout_ref.actor.ppo_mini_batch_size=$TRAIN_PROMPT_BSZ
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE
     actor_rollout_ref.actor.use_dynamic_bsz=True
+    actor_rollout_ref.actor.fsdp_config.param_offload=True
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True
 )
 
 ROLLOUT=(
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE
     actor_rollout_ref.rollout.tensor_model_parallel_size=1
-    actor_rollout_ref.rollout.name=$rollout_name
+    actor_rollout_ref.rollout.name=$ROLLOUT_NAME
     actor_rollout_ref.rollout.gpu_memory_utilization=0.3
     actor_rollout_ref.rollout.n=1
-)
-
-REF=(
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=$MICRO_BATCH_SIZE # Teacher batch size
 )
 
 ALGORITHM=(
@@ -86,10 +90,10 @@ ALGORITHM=(
 )
 
 TRAINER=(
-    trainer.logger='["console","wandb"]'
-    trainer.project_name=$project_name
-    trainer.experiment_name=$exp_name
-    trainer.n_gpus_per_node=2
+    trainer.logger='["console"]'
+    trainer.project_name=$PROJECT_NAME
+    trainer.experiment_name=$EXP_NAME
+    trainer.n_gpus_per_node=$DP_SIZE
     trainer.nnodes=1
     trainer.save_freq=200
     trainer.test_freq=10
@@ -111,6 +115,5 @@ python3 -m verl.trainer.main_ppo \
     "${DISTILLATION[@]}" \
     "${ROLLOUT[@]}" \
     "${ACTOR[@]}" \
-    "${REF[@]}" \
     "${TRAINER[@]}" \
     "$@"
