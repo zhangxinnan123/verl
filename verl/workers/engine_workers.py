c3c23e59eee2dc36dbd82e34dd3caad8a5aecdbd
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-27 10:57:39 -0600

    Ruff

--- verl/workers/engine_workers.py
+++ verl/workers/engine_workers.py
@@ -43,13 +43,7 @@ from verl.utils.profiler import DistProfiler, DistProfilerExtension, ProfilerCon
 from verl.utils.py_functional import append_to_dict
 from verl.utils.tensordict_utils import maybe_fix_3d_position_ids
 from verl.utils.torch_functional import allgather_dict_into_dict
-from verl.workers.config import (
-    ActorConfig,
-    HFModelConfig,
-    RolloutConfig,
-    TrainingWorkerConfig,
-    DistillationConfig
-)
+from verl.workers.config import ActorConfig, DistillationConfig, HFModelConfig, RolloutConfig, TrainingWorkerConfig
 from verl.workers.rollout.base import BaseRollout, get_rollout_class
 from verl.workers.utils.losses import ppo_loss
 
